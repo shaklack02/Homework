c267ec0cd1f79e0196e3fdbe66bf78db37f12925
xtreme <xtreme412@gmail.com>
2024-10-03 01:31:51 +0300

    added the structure

--- OOP_6_7/oop_homework_6_7.py
+++ OOP_6_7/oop_homework_6_7.py
@@ -1,39 +1,64 @@
 # Python - OOP - Multiple Inheritance & Interfaces
 
 from abc import ABC, abstractmethod
+from enum import Enum
+class BankNames(Enum):
+    Hapoalim = "Bank Hapoalim"
+    Leomi = "Bank Leomi"
+    Discount = "Bank Discount"
 
-class Bank:
-    def __init__(self, id, name, number_of_emplyees, amount_of_revenue, amount_of_expenses, bank_customers):
-        self._bank_id = id
-        self._bank_name = name
-        self._bank_number_of_employees = number_of_emplyees
-        self._bank_amounr_of_revenue = amount_of_revenue
-        self._bank_amount_of_expenses = amount_of_expenses
-        self._bank_costomers = bank_customers # must have at lest 3 costomers
+class CompnayCuatomer:
+    def __init__(self, id, name, bank_name, amount_of_money):
+        self._companyCustomer_id = id
+        self._companyCustomer_name = name
+        self._companyCustomer_Bank_name = bank_name
+        self._companyCustomer_amount_of_money = amount_of_money
+
 
 class Customer:
-    def __init__(self):
-        pass
+    def __init__(self, id, first_name, last_name, bank_name, credit_card_number, amount_of_money):
+        self._customer_id = id
+        self._customer_first_name = first_name
+        self._customer_last_name = last_name
+        self._customer_Bank_name = bank_name
+        self._customer_credit_card_number = credit_card_number
+        self._customer_amount_of_money = amount_of_money
 
 
-    def take_payment(customer: Customer, payment: int):
+class Bank:
+    def __init__(self, id, name : BankNames, amount_of_employees, revenue, expenses, bank_customers: []):
+        self._bank_id = id
+        self._bank_name = name
+        self._bank_number_of_employees = amount_of_employees
+        self._bank_amount_of_revenue = revenue
+        self._bank_amount_of_expenses = expenses
+        self._bank_customers = bank_customers # must have at lest 3 costomers
+
+    def take_payment(self, customer: Customer, payment: int):
         # take payment from existing customer
         # (you should check if this customer is really a customer of the bank first)
         pass
 
-    def increase_revenue(reveneue_to_add: int):
+    def increase_revenue(self, revenue_to_add: int):
         # - increase the bank revenue by the amount passed as reveneue_to_add
         pass
 
-    def increase_expenses(expenses_to_increase: int):
+    def increase_expenses(self, expenses_to_increase: int):
         # -  increase the bank expenses by the amount passed as expensesToAdd
         pass
 
-    def calculate_customer_money():
+    def calculate_customer_money(self):
         # which should update the bank revenue according to the result of this calculation
         # However, each specific bank handle the revenue calculation
         # differently:
         pass
+
+if __name__ == '__main__':
+    # main thread
+    pass
+    # Bank_Hapoalim =
+    # Bank_Leomi =
+    # Bank_Discount =
 
 
 
